1452392dc1ebc963c26b350505386164081f9f08
shackste <shackste@physnet.uni-hamburg.de>
2021-03-09 09:17:04 +0100

    typo

--- python_modules/file_system.py
+++ python_modules/file_system.py
@@ -8,7 +8,7 @@ import os
 
 root = "/drive/MyDrive/FHNW/galaxy_generator/"
 
-## the following files need to be downloaded to your google drive in root folder defined above
+
 ## download from https://www.kaggle.com/c/galaxy-zoo-the-galaxy-challenge/data
 file_galaxy_images = root + "galaxyzoo_data_cropped_nonnormalized.npy"
 file_galaxy_labels = root + "training_solutions_rev1.csv"
