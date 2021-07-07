0d565db34a4b5c1c5f72e7c4bfef048b2d2c5b2f
shackste <shackst3@gmail.com>
2021-07-07 16:12:04 +0200

    automately use cuda if available

--- python_modules/dataset.py
+++ python_modules/dataset.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 from pandas import read_csv
-from torch import from_numpy
+import  torch
 from torchvision.transforms import Compose, CenterCrop, ToTensor, RandomAffine, Resize, RandomVerticalFlip, RandomHorizontalFlip, RandomErasing, ToPILImage
 from torch.utils.data import DataLoader, Dataset, Subset
 from sklearn.model_selection import train_test_split
@@ -18,7 +18,7 @@ def get_x_train():
     x_train = np.load(file_galaxy_images)  ## (N_samples,dim,dim,colors)
     x_train = x_train/255.0 ## rescale to 0<x<1
     x_train = np.rollaxis(x_train, -1, 1)  ## pytorch: (colors,dim,dim)
-    x_train = from_numpy(x_train)
+    x_train = torch.from_numpy(x_train)
     return x_train
 
 # hierarchical galaxy labels
@@ -27,7 +27,7 @@ def get_labels_train():
     ## for now, only use top level labels
     ##    labels_train = df_galaxy_labels[df_galaxy_labels.columns[1:4]].values
     labels_train = df_galaxy_labels[df_galaxy_labels.columns[1:]].values
-    labels_train = from_numpy(labels_train).float()
+    labels_train = torch.from_numpy(labels_train).float()
     return labels_train
 
 
@@ -69,14 +69,15 @@ class DataSet(Dataset):
         return img, label
 
 class MakeDataLoader:
-    def __init__(self, test_size=0.1, random_state=2):
+    def __init__(self, test_size=0.1, random_state=2, device=torch.device("cpu")):
+        self.device = device
         dataset = DataSet()
         train_idx, test_idx = train_test_split(list(range(len(dataset))), test_size=test_size, random_state=random_state)
         self.dataset_train = Subset(dataset, train_idx)
         self.dataset_test = Subset(dataset, test_idx)
 
     def get_data_loader_train(self, batch_size=64):
-        return DataLoader(self.dataset_train, batch_size=batch_size, shuffle=True, drop_last=True )
+        return DataLoader(self.dataset_train, batch_size=batch_size, shuffle=True, drop_last=True, pin_memory=self.device==torch.device("cuda"))
 
     def get_data_loader_test(self, batch_size=64):
-        return DataLoader(self.dataset_test, batch_size=batch_size, shuffle=True, drop_last=True )
+        return DataLoader(self.dataset_test, batch_size=batch_size, shuffle=True, drop_last=True, pin_memory=self.device==torch.device("cuda"))
